f61f3e029fcb516196ef4edcb269e0cc0bcb724f
HichiHachi <I_Itachi_360_I@hotmail.co.uk>
2014-06-11 21:35:36 +0100

    Added camera Control

---
+++ SpaceGame/Assets/CameraControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	public GameObject player;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -1);
+		if (Input.GetAxis ("Mouse ScrollWheel") > 0) {
+			if(Camera.main.orthographicSize>5)Camera.main.orthographicSize--;
+			Debug.Log("Forward");
+		}
+		if (Input.GetAxis ("Mouse ScrollWheel") < 0) {
+			if(Camera.main.orthographicSize<25)Camera.main.orthographicSize++;
+			Debug.Log("Back");
+		}
+	}
+}
