1ee0ea428b4533c8ba8fc41920a2dfff081fba66
HichiHachi <I_Itachi_360_I@hotmail.co.uk>
2014-06-11 18:14:40 +0100

    Ship rotation update

--- SpaceGame/Assets/Scripts/ShipMovement.cs
+++ SpaceGame/Assets/Scripts/ShipMovement.cs
@@ -4,12 +4,11 @@ using System.Collections;
 public class ShipMovement : MonoBehaviour {
 
 	public GameObject ship;
-	float rotation = 0.8f;
+	public GameObject ship2;
+	float maneuverability = 0.8f;
 	Vector3 position;
 	Vector2 diff;
 	float distance;
-	float Radius = 25.0f;
-
 	// Use this for initialization
 	void Start () {
 	
@@ -18,17 +17,12 @@ public class ShipMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		ShipRotation ();	
-		MouseRadius ();
+		Screen.showCursor = false;
 	}
 
 	void ShipRotation(){
 		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		ship.transform.rotation = Quaternion.LookRotation (Vector3.forward, mousePos - transform.position * Time.deltaTime);
-	
-	}
-
-	void MouseRadius(){
-
-
+		Quaternion rot = Quaternion.LookRotation (Vector3.forward, (mousePos - ship.transform.position * 2.0f));
+		ship.transform.rotation = Quaternion.Lerp (ship.transform.rotation, rot, Time.deltaTime*maneuverability);
 	}
 }
