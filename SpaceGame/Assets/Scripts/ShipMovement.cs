3dc26d23cf68b8d043f4226e2f0f8422181aa466
HichiHachi <I_Itachi_360_I@hotmail.co.uk>
2014-06-11 21:21:17 +0100

    ShipMovement

--- SpaceGame/Assets/Scripts/ShipMovement.cs
+++ SpaceGame/Assets/Scripts/ShipMovement.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class ShipMovement : MonoBehaviour {
 
 	public GameObject ship;
-	public GameObject ship2;
 	float maneuverability = 0.8f;
 	Vector3 position;
 	Vector2 diff;
 	float distance;
+	float SlowDownSpeed = 0.001f;
+	float ForwardSpeed = 0.005f;
+	float velocity;
 	// Use this for initialization
 	void Start () {
 	
@@ -16,7 +18,8 @@ public class ShipMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		ShipRotation ();	
+		ShipRotation ();
+		MoveShip ();
 		Screen.showCursor = false;
 	}
 
@@ -24,5 +27,17 @@ public class ShipMovement : MonoBehaviour {
 		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		Quaternion rot = Quaternion.LookRotation (Vector3.forward, (mousePos - ship.transform.position * 2.0f));
 		ship.transform.rotation = Quaternion.Lerp (ship.transform.rotation, rot, Time.deltaTime*maneuverability);
+	}
+
+	void MoveShip(){
+		if (Input.GetMouseButton (1)) {
+			velocity +=ForwardSpeed;
+			Debug.Log("RightClick");
+		}
+		if (Input.GetMouseButton (0)) {
+			if (velocity > 0)velocity -= SlowDownSpeed;
+		}
+		ship.rigidbody2D.transform.position += ship.rigidbody2D.transform.up*velocity;
+		if(velocity>0)velocity -= 0.0001f;
 	}
 }
